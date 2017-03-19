19607d38e1b872a6473d1f9909fd946d0e783205
Dev Saini <Dev Saini>
2017-03-20 00:41:56 +0530

    Shell Script for TDD

--- Ellipses/StringUtils_TestDrivenScript.sh
+++ Ellipses/StringUtils_TestDrivenScript.sh
@@ -30,83 +30,83 @@ function executeTests()
 	# Compile and generate Executable 
 	gcc StringUtilTest.c StringUtils.c -o StringUtils.exe
 
-  #-------------------------- TEST CASE 1 ------------------------------#
+  #-------------------------- TEST CASE 1 ----------------------------------#
 	expected="Indonesia"
 	output=$(./StringUtils.exe -maxlength 12 -lastnchars 3 "Indonesia")
 	result=$(checkIfEqual $expected $output)
 
 	printTestCaseResult $1 $testnumber $result
-  #---------------------------------------------------------------------#
+  #-------------------------------------------------------------------------#
 	((testnumber++))
   
 
-  #-------------------------- TEST CASE 2 --------------------------------#
+  #-------------------------- TEST CASE 2 -------------------------------------#
 	expected="Indonesia123"
 	output=$(./StringUtils.exe -maxlength 12 -lastnchars 3 "Indonesia123")
 	result=$(checkIfEqual $expected $output)
 
 	printTestCaseResult $1 $testnumber $result
-  #-----------------------------------------------------------------------#
+  #----------------------------------------------------------------------------#
 	((testnumber++))
 
 
-  #-------------------------- TEST CASE 3 ---------------------------------#
+  #-------------------------- TEST CASE 3 --------------------------------------#
 	expected="Indone...234"
 	output=$(./StringUtils.exe -maxlength 12 -lastnchars 3 "Indonesia1234")
 	result=$(checkIfEqual $expected $output)
 
 	printTestCaseResult $1 $testnumber $result
-  #------------------------------------------------------------------------#
+  #-----------------------------------------------------------------------------#
   	((testnumber++))
 
 
-  #-------------------------- TEST CASE 4 ------------------------------#
+  #-------------------------- TEST CASE 4 ---------------------------------#
 	expected="Ind..."
 	output=$(./StringUtils.exe -maxlength 6 -lastnchars 0 "Indonesia")
 	result=$(checkIfEqual $expected $output)
 
 	printTestCaseResult $1 $testnumber $result
-  #---------------------------------------------------------------------#
+  #------------------------------------------------------------------------#
 	((testnumber++))
 
 
-  #-------------------------- TEST CASE 5 ------------------------------#
+  #-------------------------- TEST CASE 5 ---------------------------------#
 	expected="...nesia"
 	output=$(./StringUtils.exe -maxlength 8 -lastnchars 7 "Indonesia")
 	result=$(checkIfEqual $expected $output)
 
 	printTestCaseResult $1 $testnumber $result
-  #---------------------------------------------------------------------#
+  #------------------------------------------------------------------------#
 	((testnumber++))
 
 
-  #-------------------------- TEST CASE 6 ------------------------------#
+  #-------------------------- TEST CASE 6 ---------------------------------#
 	expected="Error!"
 	output=$(./StringUtils.exe -maxlength 8 -lastnchars 9 "Indonesia")
 	result=$(checkIfEqual $expected $output)	
 
 	printTestCaseResult $1 $testnumber $result
-  #---------------------------------------------------------------------#
+  #------------------------------------------------------------------------#
 	((testnumber++))
 
 
-  #-------------------------- TEST CASE 7 ------------------------------#
+  #-------------------------- TEST CASE 7 ----------------------------------#
 	expected="Error!"
 	output=$(./StringUtils.exe -maxlength -8 -lastnchars 3 "Indonesia")
 	result=$(checkIfEqual $expected $output)	
 
 	printTestCaseResult $1 $testnumber $result
-  #---------------------------------------------------------------------#
+  #-------------------------------------------------------------------------#
 	((testnumber++))
 
 
-  #-------------------------- TEST CASE 8 ------------------------------#
+  #-------------------------- TEST CASE 8 ----------------------------------#
 	expected="Error!"
 	output=$(./StringUtils.exe -maxlength 8 -lastnchars -3 "Indonesia")
 	result=$(checkIfEqual $expected $output)	
 
 	printTestCaseResult $1 $testnumber $result
-  #---------------------------------------------------------------------#
+  #-------------------------------------------------------------------------#
 }
 
 startScript
